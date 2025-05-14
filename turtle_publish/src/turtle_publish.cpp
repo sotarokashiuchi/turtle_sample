a3e4689606bd929c7892b83a3dbb4acc6f6a22c5
Kousuke Okabe <kousuke.okabe@ieee.org>
2025-05-14 09:55:04 +0000

    turtle_subscribe

--- turtle_publish/src/turtle_publish.cpp
+++ turtle_publish/src/turtle_publish.cpp
@@ -1,24 +1,32 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 
-using namespace std::chrono_literals;
-
 int main(int argc, char **argv){
+    // Initialize the ROS2
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("turtle_publisher");
 
+    // Create a node
+    auto node = rclcpp::Node::make_shared("turtle_publish");
+
+    // Create a publisher
     auto pub_twist = node->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
 
-    rclcpp::WallRate loop(0.5);
+    // Create a timer to publish messages at a fixed rate
+    rclcpp::WallRate loop(0.5); // 2Hz
 
     while(rclcpp::ok()){
+        // Create a message
         auto msg = geometry_msgs::msg::Twist();
         msg.linear.x = 1.0;
         msg.angular.z = 1.0;
 
+        // Display the message in the console
         RCLCPP_INFO(node->get_logger(), "Publishing: '%f'", msg.linear.x);
+
+        // Publish the message
         pub_twist->publish(msg);
 
+        // Spin the node to process callbacks
         loop.sleep();
     }
 
